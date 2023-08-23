af548d2691bd8e70dcce001fa8bba3749a279283
dvmrqs <142408282+dvmrqs@users.noreply.github.com>
2023-08-23 13:57:02 +0000

    envio para o github

---
+++ aulas/aula01/meuprograma.c
@@ -0,0 +1,17 @@
+/**********************************
+  Autor: Davi Marques
+  // Data: 23/08/2023
+
+  Programa basico em C
+***********************************/  
+#include <stdio.h>
+#include <math.h> 
+
+//funcao principal
+
+int main() { 
+printf("ola pessoal!\n");
+printf("raiz quadrada de 2 é %f\n", sqrt(2));
+  
+  return 0; 
+} 
