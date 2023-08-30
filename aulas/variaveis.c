f2a7bb0f1b5e1f5ee7082c18516cf9802b287a62
dvmrqs <142408282+dvmrqs@users.noreply.github.com>
2023-08-30 13:36:34 +0000

    codigo da aula 2

---
+++ aulas/variaveis.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+  char caracter; // ou '\0' '\n', '\t'; // 'a', 'b', 'c', -128 a 127 - 1 byte
+
+  unsigned char caracter_sem_sinal; // 0 a 255
+
+  char i = 0;
+  char j = 0;
+
+  char string[10]; // "um texto"
+  
+  int inteiro; // -1, 0, 1... -2147483648 a 2147483647 - 4 bytes
+
+  unsigned int inteiro_sem_sinal; // 0 a 4294967295
+
+  short int inteiro_curto; // -32768 a 32767 - 2 bytes
+
+  unsigned short int inteiro_curto_sem_sinal; // 0 a 65535 
+  
+  float flutuante; // 0.123456f - 4 bytes
+  
+  double duplo; // 0.123456789012345 - 8 bytes 
+  
+  void nada; // 1 byte
+
+  return 0;
+  } 
---
+++ praticas/pratica01/questao02.c
@@ -0,0 +1,14 @@
+/*Faca um programa em C que calcule a aerea de um triangulo. Considere base e altura numeros inteiros maior que 0
+  */
+
+  #include <stdio.h>
+
+  int main () {
+  unsigned int base = 5;
+  unsigned int altura = 8;
+
+   float area = base * altura / 2;
+
+printf("a area é %f\n", area);
+return 0;
+}
