f5a4f6cc30c4f478f8b367b9e8a30d9334cc5cd3
dvmrqs <142408282+dvmrqs@users.noreply.github.com>
2023-09-18 21:29:32 +0000

    .

--- praticas/pratica01/questao01.c
+++ praticas/pratica01/questao01.c
@@ -1,4 +1,4 @@
-/*
+
 
 
 #include <stdio.h> 
@@ -7,9 +7,9 @@ int main() {
   float nota_a1;
   float nota_a2;
 
-  printf("Entre com a nota do primeiro bimestre");
+  printf("Entre com a nota do primeiro bimestre ");
   scanf("%f", &nota_a1);
-  printf("Entre com a nota do segundo bimestre");
+  printf("Entre com a nota do segundo bimestre ");
   scanf("%f", &nota_a2);
 
   float media = 0.4 * nota_a1 + 0.6 * nota_a2;
